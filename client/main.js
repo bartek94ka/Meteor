e555a16248d29c4c87ded9fb1b44dca142ed0d14
bartek94ka <bartosz.kaczorowski@student.put.poznan.pl>
2018-05-21 20:49:47 +0200

    Adding form to add new player

--- client/main.js
+++ client/main.js
@@ -1,16 +1,14 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
-import './main.html';
-// Set up a collection to contain player information. On the server,
-// it is backed by a MongoDB collection named "players".
+import { Meteor } from 'meteor/meteor';
+import { Session } from 'meteor/session';
 
 Players = new Mongo.Collection("players");
+// NewPlayer = new Mongo.
 
 if (Meteor.isClient) {
   Template.leaderboard.helpers({
     players: function () {
-      
       return Players.find({}, { sort: { score: -1, name: 1 } });
     },
     selectedName: function () {
@@ -36,19 +34,15 @@ if (Meteor.isClient) {
       Session.set("selectedPlayer", this._id);
     }
   });
-}
 
-// On server startup, create some players if the database is empty.
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    if (Players.find().count() === 0) {
-      var names = ["Ada Lovelace", "Grace Hopper", "Marie Curie",
-                   "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"];
-      _.each(names, function (name) {
-        Players.insert({
-          name: name,
-          score: Math.floor(Random.fraction() * 10) * 5
-        });
+  Template.addPlayer.events({
+    'submit form': function (event) {
+      event.preventDefault();
+      var playerNameVar = event.target.playerName.value;
+      console.log(playerNameVar);
+      Players.insert({
+        name: playerNameVar,
+        score: 0
       });
     }
   });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,24 @@
 import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
+
+Players = new Mongo.Collection("players");
 
 Meteor.startup(() => {
   // code to run on server at startup
+    if (Meteor.isServer) {
+        Meteor.startup(function () {
+            if (Players.find().count() === 0) {
+            var names = ["Ada Lovelace", "Grace Hopper", "Marie Curie",
+                        "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"];
+            _.each(names, function (name) {
+                Players.insert({
+                name: name,
+                score: Math.floor(Random.fraction() * 10) * 5
+                });
+            });
+            console.log(Players);
+            }
+        });
+    }
 });
+
